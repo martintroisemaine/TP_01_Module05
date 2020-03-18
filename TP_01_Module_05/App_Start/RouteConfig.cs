59ba559efbb94ee05d3ce13917b389d5cd62c24c
martintroisemaine <martin.troisemaine2018@campus-eni.fr>
2020-03-18 17:32:34 +0100

    Initial commit

---
+++ TP_01_Module_05/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TP_01_Module_05
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Chats",
+                url: "Chats",
+                defaults: new { controller = "Chat", action = "Index" });
+        }
+    }
+}
---
+++ TP_01_Module_05/Controllers/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_01_Module_05.Models;
+
+namespace TP_01_Module_05.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(Chat.GetMeuteDeChats());
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Chat.GetMeuteDeChats()[id]);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(Chat.GetMeuteDeChats()[id]);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // Chat.GetMeuteDeChats().Remove(Chat.GetMeuteDeChats()[id]);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
